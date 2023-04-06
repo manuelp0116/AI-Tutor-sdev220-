b1ec4cd88a79a1bfb434687ed4fcafbddba73818
Frank Mcalister <44241157+Frankbbg@users.noreply.github.com>
2023-04-06 15:10:38 -0400

    A (very) Basic Layout
    Created a basic layout that can be built off of when branching.

---
+++ app.py
@@ -0,0 +1,36 @@
+'''
+APP DESCRIPTION HERE
+'''
+
+# Standard libraries we might use later on
+import random
+import math
+import time
+
+# User Created Classes
+from ai import LanguageModel # The AI class
+from gui import UI # The CustomTkinter class
+
+#------------------------------------------------------------
+# OpenAI API functions go here
+
+
+
+#------------------------------------------------------------
+# Whisper speech transcription functions go here
+
+
+
+#------------------------------------------------------------
+# Tkinter Functions go here
+
+
+
+#------------------------------------------------------------
+# Basic functions go here
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
