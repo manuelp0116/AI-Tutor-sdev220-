ec7d9e20c3621c1cb6fd996af5aa7007b97c6e3b
manuelp1601 <92879473+manuelp1601@users.noreply.github.com>
2023-05-08 15:21:25 -0400

    Create storageSolutions.py
    create simple storage solutions, needs work to complete and fix bugs.

---
+++ storageSolutions.py
@@ -0,0 +1,96 @@
+import openai
+import datetime
+import os
+
+import json
+
+openai.api_key = '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'
+
+
+response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages = [
+    {"role": "system", "content": "you are a quiz creation app, your answers will be formatted in a json file"},
+    {"role": "user", "content": "make a 3 question quiz about elemetary level astronomy"},
+    ]
+)
+print(response)
+
+class storagesolutions:
+    
+    '''
+    Storage solutions,
+    
+    functions housekeeping, save quiz, save chat
+    '''
+    
+    def housekeep(self):
+        #use this function at the start of the app to create and get folders ready, it will create the necessary folders for the stuff that needs to be saved, and will check for those same folders in the future
+        if os.path.exists("quizSTG") and os.path.exists("chatSTG"):
+            print("folder exist")
+        
+        else:
+            quiz_dir = "QuizSTG" #name is up to changes
+            chat_dir = ""
+            
+            path = os.path.join(directory) #creates name of the folder
+            
+            os.mkdir(path)
+            
+    def saveQuiz(self, subject, grade, response):
+        #will save output from AI in a json file, parsing could be necesary, naming conventions for the files are up to change.
+        file_name = str(subject) + "quiz" + str(grade) #file name, could add datetime module to name the files in a more personalized manner
+        file_path = file_name + ".json"
+        file_dir = os.path.dirname("quizSTG")
+        with open(file_path, 'w') as fp:
+            json.dump(response,fp)
+        
+        fp.close
+        
+    def saveChat(self, chat, subject, grade):
+        #
+        file_name = 'chat' + str(subject) + str(grade)
+        file_path = file_name + ".txt"
+        file_dir = os.path.dirname("chatSTG")
+        with open(file_path, "w") as fp:
+            fp.write
+        fp.close
+    
+
+
+
+'''
+def saveQuiz(response, subject):
+    
+  
+    if os.path.exists("quizSTG"): #checks fro folder to store the quizzes
+
+        filename = "quiz" + str(1) #creates file name, need to find a way to create diff names every time 
+        file_path = filename + ".json"
+        file_dir = os.path.dirname("QuizSTG")
+        with open(file_path, 'w') as fp:
+            json.dump(response, fp)
+    else:    
+        directory = "QuizSTG"
+        # Path
+        path = os.path.join(directory)
+  
+        # Create the directory
+        os.mkdir(path)
+
+def saveChat(response):
+    if os.path.exists("ChatSTG"):
+    
+        count = 1
+        file_path = "Chat" + str(count)
+        count += 1
+        file_dir = os.path.dirname("ChatSTG")
+        with open(file_path, 'w') as fp:
+            json.dump(response, fp)
+    else:    
+        directory = "ChatSTG"
+        # Path
+        path = os.path.join(directory)
+  
+        # Create the directory
+        os.mkdir(path)
+'''
+    
